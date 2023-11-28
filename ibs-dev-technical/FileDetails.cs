f431cc25bd3cf3df973793f4eb8b0ff34486b95e
David Barredo <datdat@rdmsinc.com>
2023-11-28 21:04:40 +0800

    add file details and revision classes

---
+++ ibs-dev-technical/FileDetails.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ibs_dev_technical
+{
+    public class FileDetails
+    {
+        public string FileName { get; set; }
+        public int Version_ID { get; set; }
+        public bool IsRevision { get; set; }
+    }
+}
---
+++ ibs-dev-technical/Revisions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ibs_dev_technical
+{
+    public class Revisions
+    {
+        public FileDetails Check()
+        {
+            //check revision and return file details
+            return null;
+        }
+    }
+}
